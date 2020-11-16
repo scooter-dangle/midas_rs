7e0fabe3a088ed001d8af513f1956eaffae27f16
Scott Steele <scottlsteele@gmail.com>
2020-11-16 00:49:17 -0500

    test: demonstrate useful alpha decay
    Previous example used the default value of 0.6 from the original source,
    which is incredibly aggressive when applied every second.

--- python/test.py
+++ python/test.py
@@ -5,7 +5,15 @@ from midas import (
 import csv
 
 if __name__ == "__main__":
-    midas = MidasR()
+    # alpha is the decay value.
+    #
+    # With every increment in the time value, all the stored bucket
+    # values are multiplied by this number.
+    #
+    # The alpha value below means that a value of 1.0 at t_0 will decay
+    # to 0.8 by t_30
+    alpha = 0.8 ** (1.0 / 30.0)
+    midas = MidasR(alpha=alpha)
     with open("in.csv") as infile, open("test.out.csv", "w") as outfile:
         reader = csv.reader(infile)
         writer = csv.writer(outfile)
